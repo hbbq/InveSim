7fa6047574e1d9b1b079e8ff72f9dc75f282ae8c
Henrik Bergqvist <Henrik.Bergqvist@alektumgroup.com>
2020-11-10 08:40:53 +0100

    New signals

--- InveSim.App/Program.cs
+++ InveSim.App/Program.cs
@@ -366,7 +366,7 @@ namespace InveSim.App
                     // V45 2020-10-26
                     sig.Add("EOLU B  ", "20201026", "20201030");
                     sig.Add("HIQ     ", "20201026", null);
-                    sig.Add("FABG    ", "20201027", null);
+                    sig.Add("FABG    ", "20201027", "20201110");
                     sig.Add("LATO B  ", "20201027", "20201104");
                     sig.Add("NET B   ", "20201027", null);
                     sig.Add("INTRUM  ", "20201028", null);
